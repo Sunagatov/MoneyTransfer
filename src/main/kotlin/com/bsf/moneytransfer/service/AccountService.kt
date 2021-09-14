aa5e14c9be274a0281ca07bf2d98133dd355e8b4
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2021-09-15 00:03:12 +0300

    AddMoney, WithdrawMoney account operations  were added

--- src/main/kotlin/com/bsf/moneytransfer/service/AccountService.kt
+++ src/main/kotlin/com/bsf/moneytransfer/service/AccountService.kt
@@ -1,9 +1,11 @@
 package com.bsf.moneytransfer.service
 
 import com.bsf.moneytransfer.model.Account
+import com.bsf.moneytransfer.model.AccountUpdateDetails
 import com.bsf.moneytransfer.model.MoneyTransferDetails
 import com.bsf.moneytransfer.repository.AccountRepository
 import com.bsf.moneytransfer.utils.validateAccountDetails
+import com.bsf.moneytransfer.utils.validateAccountUpdateDetails
 import com.bsf.moneytransfer.utils.validateTransferDetails
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Isolation
@@ -15,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional
  * @author Zufar Sunagatov (zufar.sunagatov@gmail.com)
  */
 @Service
-class AccountService(val accountRepository: AccountRepository) {
+class AccountService(private val accountRepository: AccountRepository) {
 
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     fun transferMoney(moneyTransferDetails: MoneyTransferDetails) {
@@ -36,5 +38,29 @@ class AccountService(val accountRepository: AccountRepository) {
     fun getAccountDetails(id: Long) = accountRepository.getById(id)
 
     @Transactional(isolation = Isolation.REPEATABLE_READ)
-    fun updateAccount(acc: Account) = accountRepository.save(acc)
+    fun updateAccount(account: Account) = accountRepository.save(account)
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    fun addMoney(accountUpdateDetails: AccountUpdateDetails) = with(accountUpdateDetails) {
+        validateAccountUpdateDetails(accountUpdateDetails)
+
+        val accountDetails = getAccountDetails(accountId)
+        accountDetails.balance += amount
+
+        accountRepository.save(accountDetails)
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    fun withdrawMoney(accountUpdateDetails: AccountUpdateDetails) = with(accountUpdateDetails) {
+        validateAccountUpdateDetails(accountUpdateDetails)
+        val accountDetails = getAccountDetails(accountId)
+
+        accountDetails.balance -= amount
+        validateAccountDetails(accountDetails)
+
+        accountRepository.save(accountDetails)
+    }
+
+    @Transactional
+    fun createAccount(account: Account) = accountRepository.save(account)
 }
--- src/main/kotlin/com/bsf/moneytransfer/utils/Utils.kt
+++ src/main/kotlin/com/bsf/moneytransfer/utils/Utils.kt
@@ -1,6 +1,7 @@
 package com.bsf.moneytransfer.utils
 
 import com.bsf.moneytransfer.model.Account
+import com.bsf.moneytransfer.model.AccountUpdateDetails
 import com.bsf.moneytransfer.model.MoneyTransferDetails
 import java.math.BigDecimal
 
@@ -11,5 +12,9 @@ fun validateTransferDetails(moneyTransferDetails: MoneyTransferDetails) =
 fun validateAccountDetails(accountDetails: Account) =
     if (isAmountNegative(accountDetails.balance))
         throw Exception("Account details are invalid. Balance is negative {${accountDetails.balance}}") else {}
+
+fun validateAccountUpdateDetails(accountDetails: AccountUpdateDetails) =
+    if (isAmountNegative(accountDetails.amount))
+        throw Exception("Account update details are invalid. Amount is negative {${accountDetails.amount}}") else {}
 
 fun isAmountNegative(amount: BigDecimal) = amount < BigDecimal.ZERO
