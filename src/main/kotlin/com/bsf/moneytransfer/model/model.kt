0e47b174e8d9899cd0032e1e05d606c8d289e7c7
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2021-09-14 23:37:24 +0300

    Models were updated

--- src/main/kotlin/com/bsf/moneytransfer/model/model.kt
+++ src/main/kotlin/com/bsf/moneytransfer/model/model.kt
@@ -3,12 +3,24 @@ package com.bsf.moneytransfer.model
 import java.math.BigDecimal
 import javax.persistence.*
 
+/**
+ * Information about an account
+ * */
 @Entity
 @Table(name = "accounts")
 data class Account(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0,
-                   val balance: BigDecimal = BigDecimal.ZERO)
+                   var balance: BigDecimal = BigDecimal.ZERO)
 
-data class TransferDetails(val accountFromId: Long,
-                    val accountToId: Long,
-                    val sum: BigDecimal,
-                    val description: String)
+/**
+ * Information about a money transfer
+ * */
+data class MoneyTransferDetails(val accountFromId: Long,
+                                val accountToId: Long,
+                                val amount: BigDecimal,
+                                val description: String)
+
+/**
+ * Information about an account update
+ * */
+data class AccountUpdateDetails(val accountId: Long,
+                                val amount: BigDecimal)
