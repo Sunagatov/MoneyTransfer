6b0a43ca8ebb01894a79e376145136ae408e8cb4
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2021-09-15 23:08:40 +0300

    SpringFoxConfig class braces were deleted

--- src/main/kotlin/com/bsf/moneytransfer/configuration/Configuration.kt
+++ src/main/kotlin/com/bsf/moneytransfer/configuration/Configuration.kt
@@ -7,15 +7,19 @@ import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 
-
+/**
+ * Swagger configuration
+ *
+ * @author Zufar Sunagatov (zufar.sunagatov@gmail.com)
+ */
 @Configuration
 class SpringFoxConfig {
+
     @Bean
-    fun api(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
+    fun api(): Docket =
+        Docket(DocumentationType.SWAGGER_2)
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
             .build()
-    }
 }
