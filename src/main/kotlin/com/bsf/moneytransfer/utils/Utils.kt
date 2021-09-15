135f05fed4c9b7adffd7cbbba84a2705354804fa
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2021-09-16 00:11:22 +0300

    logging was added to validation methods

--- src/main/kotlin/com/bsf/moneytransfer/utils/Utils.kt
+++ src/main/kotlin/com/bsf/moneytransfer/utils/Utils.kt
@@ -6,22 +6,35 @@ import com.bsf.moneytransfer.exception.InvalidAccountDetailsException
 import com.bsf.moneytransfer.exception.InvalidAccountUpdateDetailsException
 import com.bsf.moneytransfer.exception.InvalidTransferDetailsException
 import com.bsf.moneytransfer.entity.Account
+import com.bsf.moneytransfer.service.impl.AccountServiceImpl
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.math.BigDecimal
 
+var log: Logger = LoggerFactory.getLogger(AccountServiceImpl::class.java)
+
 fun validateTransferDetails(moneyTransferDetails: MoneyTransferDetails) {
-    if (isAmountNegative(moneyTransferDetails.amount))
-        throw InvalidTransferDetailsException("Transfer details are invalid. Amount is negative {${moneyTransferDetails.amount}}")
+    if (isAmountNegative(moneyTransferDetails.amount)) {
+        val errorMessage = "Transfer details is invalid. Amount is negative {${moneyTransferDetails.amount}}"
+        log.error(errorMessage)
+        throw InvalidTransferDetailsException(errorMessage)
+    }
 }
 
 fun validateAccountDetails(accountDetails: Account) {
-    if (isAmountNegative(accountDetails.balance))
-        throw InvalidAccountDetailsException("Account details are invalid. Balance is negative {${accountDetails.balance}}")
+    if (isAmountNegative(accountDetails.balance)) {
+        val errorMessage = "Account details is invalid. Balance is negative {${accountDetails.balance}}"
+        log.error(errorMessage)
+        throw InvalidAccountDetailsException(errorMessage)
+    }
 }
 
-
 fun validateAccountUpdateDetails(accountDetails: AccountUpdateDetails) {
-    if (isAmountNegative(accountDetails.amount))
-        throw InvalidAccountUpdateDetailsException("Account update details are invalid. Amount is negative {${accountDetails.amount}}")
+    if (isAmountNegative(accountDetails.amount)) {
+        val errorMessage = "Account update details is invalid. Amount is negative {${accountDetails.amount}}"
+        log.error(errorMessage)
+        throw InvalidAccountUpdateDetailsException(errorMessage)
+    }
 }
 
 fun isAmountNegative(amount: BigDecimal) = amount < BigDecimal.ZERO
