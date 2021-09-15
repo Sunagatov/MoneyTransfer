2319e87a07b704e0637566ee41a6fcc3dbc5d8a8
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2021-09-15 09:54:39 +0300

    Swagger and liquibase support was added

---
+++ src/main/kotlin/com/bsf/moneytransfer/Configuration.kt
@@ -0,0 +1,21 @@
+package com.bsf.moneytransfer
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+
+@Configuration
+class SpringFoxConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+    }
+}
