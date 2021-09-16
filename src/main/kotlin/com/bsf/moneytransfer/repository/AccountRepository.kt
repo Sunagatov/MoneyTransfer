a7c6270edc4e0d1612721f1437d6faef348c42f1
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2021-09-16 09:11:44 +0300

    AccountRepository java doc was updated

--- src/main/kotlin/com/bsf/moneytransfer/repository/AccountRepository.kt
+++ src/main/kotlin/com/bsf/moneytransfer/repository/AccountRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 /**
- * Account DAO
+ * CRUD operations on an account in database
  *
  * @author Zufar Sunagatov (zufar.sunagatov@gmail.com)
  */
