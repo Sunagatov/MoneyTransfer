2ce531760567e8a6dcb956c577b54c36e55ca55f
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2021-09-16 10:44:13 +0300

    ResponseEntity was replaced by @ResponseBody

--- src/main/kotlin/com/bsf/moneytransfer/controller/AccountController.kt
+++ src/main/kotlin/com/bsf/moneytransfer/controller/AccountController.kt
@@ -6,8 +6,6 @@ import com.bsf.moneytransfer.entity.Account
 import com.bsf.moneytransfer.service.AccountService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 /**
@@ -20,29 +18,26 @@ import org.springframework.web.bind.annotation.*
 class AccountController(private val accountService: AccountService) {
 
     /**
-     * Get an existed account details
+     * Get an existing account details
      *
-     * @param id an existed account id
+     * @param id an existing account id
      * @return an account details
      */
     @GetMapping("/{id}")
-    @Operation(summary = "Get an existed account details")
-    fun getAccountDetails(@Parameter(description = "Existed account id") @PathVariable id: Long): ResponseEntity<Account> {
-        val accountDetails = accountService.getAccountDetails(id)
-        return ResponseEntity.ok(accountDetails)
-    }
+    @ResponseBody
+    @Operation(summary = "Get an existing account details")
+    fun getAccountDetails(@Parameter(description = "existing account id") @PathVariable id: Long): Account =
+        accountService.getAccountDetails(id)
 
     /**
-     * Get all existed accounts
+     * Get all existing accounts
      *
-     * @return all existed accounts
+     * @return all existing accounts
      */
     @GetMapping
-    @Operation(summary = "Get all existed accounts")
-    fun getAllAccounts(): ResponseEntity<List<Account>> {
-        val allAccounts = accountService.getAllAccounts()
-        return ResponseEntity.ok(allAccounts)
-    }
+    @ResponseBody
+    @Operation(summary = "Get all existing accounts")
+    fun getAllAccounts(): List<Account> = accountService.getAllAccounts()
 
     /**
      * Create a new account
@@ -50,76 +45,62 @@ class AccountController(private val accountService: AccountService) {
      * @return a new account
      */
     @PostMapping
+    @ResponseBody
     @Operation(summary = "Create a new account")
-    fun createAccount(): ResponseEntity<Account> {
-        val newAccount = accountService.createAccount()
-        return ResponseEntity.ok(newAccount)
-    }
+    fun createAccount(): Account = accountService.createAccount()
 
     /**
      * Transfer money from one account to another
      *
      * @param moneyTransferDetails transfer money data
-     * @return http status with description
      */
     @PatchMapping("/transfer-money")
     @Operation(summary = "Transfer money from one account to another")
-    fun transferMoney(@Parameter(description = "Transfer money data") @RequestBody moneyTransferDetails: MoneyTransferDetails): ResponseEntity<String> {
+    fun transferMoney(@Parameter(description = "Transfer money data") @RequestBody moneyTransferDetails: MoneyTransferDetails) =
         accountService.transferMoney(moneyTransferDetails)
 
-        return ResponseEntity("Transfer ${moneyTransferDetails.amount} money with description (${moneyTransferDetails.description})" +
-                                      " from the account (id ${moneyTransferDetails.accountFromId})" +
-                                      " to the account (id ${moneyTransferDetails.accountToId}) was finished successfully", HttpStatus.OK)
-    }
-
     /**
-     * Add money to an existed account
+     * Add money to an existing account
      *
      * @param accountUpdateDetails add money data
      * @return an account details
      */
     @PatchMapping("/add-money")
-    @Operation(summary = "Add money to an existed account")
-    fun addMoney(@Parameter(description = "Add money data") @RequestBody accountUpdateDetails: AccountUpdateDetails): ResponseEntity<Account> {
-        val accountDetails = accountService.addMoney(accountUpdateDetails)
-        return ResponseEntity.ok(accountDetails)
-    }
+    @ResponseBody
+    @Operation(summary = "Add money to an existing account")
+    fun addMoney(@Parameter(description = "Add money data") @RequestBody accountUpdateDetails: AccountUpdateDetails): Account =
+        accountService.addMoney(accountUpdateDetails)
 
     /**
-     * Withdraw money from an existed account
+     * Withdraw money from an existing account
      *
      * @param accountUpdateDetails  withdraw money data
      * @return an account details
      */
     @PatchMapping("/withdraw-money")
-    @Operation(summary = "Withdraw money from an existed account by id")
-    fun withdrawMoney(@Parameter(description = "Withdraw money data") @RequestBody accountUpdateDetails: AccountUpdateDetails): ResponseEntity<Account> {
-        val accountDetails = accountService.withdrawMoney(accountUpdateDetails)
-        return ResponseEntity.ok(accountDetails)
-    }
+    @ResponseBody
+    @Operation(summary = "Withdraw money from an existing account by id")
+    fun withdrawMoney(@Parameter(description = "Withdraw money data") @RequestBody accountUpdateDetails: AccountUpdateDetails): Account =
+        accountService.withdrawMoney(accountUpdateDetails)
+
 
     /**
-     * Delete all existed accounts
+     * Delete all existing accounts
      *
      * @return http status with description
      */
     @DeleteMapping
-    @Operation(summary = "Delete all existed accounts")
-    fun deleteAllAccount(): ResponseEntity<String> {
-        accountService.deleteAllAccounts()
-        return ResponseEntity("All existed accounts were deleted successfully", HttpStatus.OK)
-    }
+    @Operation(summary = "Delete all existing accounts")
+    fun deleteAllAccount() = accountService.deleteAllAccounts()
 
     /**
-     * Delete an existed account
+     * Delete an existing account
      *
-     * @param id an existed account id
+     * @param id an existing account id
      * @return http status
      */
     @DeleteMapping("/{id}")
-    @Operation(summary = "Delete an existed account by id")
-    fun deleteAccount(@Parameter(description = "Existed account id") @PathVariable id: Long): ResponseEntity<String> {
+    @Operation(summary = "Delete an existing account by id")
+    fun deleteAccount(@Parameter(description = "existing account id") @PathVariable id: Long) =
         accountService.deleteAccount(id)
-        return ResponseEntity("Account with id=$id was deleted successfully", HttpStatus.OK)
-    }
 }
